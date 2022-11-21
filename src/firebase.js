909fe593666c9cd892ce536ad6bf4e54c6ed78bf
Ankit Seth <iam4nkit@gmail.com>
2022-11-21 11:07:22 +0530

    added firebase SDK

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuDyTmUEy4gY7Yeifs0r2a_yBFNgrf8vg",
+  authDomain: "react-firebase-376e4.firebaseapp.com",
+  projectId: "react-firebase-376e4",
+  storageBucket: "react-firebase-376e4.appspot.com",
+  messagingSenderId: "223030603375",
+  appId: "1:223030603375:web:795883137b3a587962e79a",
+  measurementId: "G-VHQNPWZL9P"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
