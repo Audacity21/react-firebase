1824c0c6ac55942557b35bf71f21ade2bdc72cba
Ankit Seth <iam4nkit@gmail.com>
2022-11-21 19:45:59 +0530

    completed authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import { Button, Card, CardContent, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react'
+import { auth } from './firebase';
+import { RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+
+  const [phone, setPhone] = useState('+91');
+  const [hasFilled, setHasFilled] = useState(false);
+  const [otp, setOtp] = useState('');
+
+  const generateRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier('recaptcha', {
+      'size': 'invisible',
+      'callback': (response) => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+        // ...
+      }
+    }, auth);
+  }
+
+  const handleSend = (event) => {
+    event.preventDefault();
+    setHasFilled(true);
+    generateRecaptcha();
+    let appVerifier = window.recaptchaVerifier;
+    signInWithPhoneNumber(auth, phone, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+      }).catch((error) => {
+        // Error; SMS not sent
+        console.log(error);
+      });
+  }
+  
+  const verifyOtp = (event) => {
+    let otp = event.target.value;
+    setOtp(otp);
+
+    if (otp.length === 6) {
+      // verifu otp
+      let confirmationResult = window.confirmationResult;
+      confirmationResult.confirm(otp).then((result) => {
+        // User signed in successfully.
+        let user = result.user;
+        console.log(user);
+        alert('User signed in successfully');
+        // ...
+      }).catch((error) => {
+        // User couldn't sign in (bad verification code?)
+        // ...
+        alert('User couldn\'t sign in (bad verification code?)');
+      });
+    }
+  }
+
+  if(!hasFilled){
+    return (
+      <div className='app__container'>
+        <Card sx={{ width: '300px'}}>
+          <CardContent sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <Typography sx={{ padding: '20px'}} variant='h5' component='div'>Enter your phone number</Typography>
+            <form onSubmit={handleSend}>
+              <TextField sx={{ width: '240px'}} variant='outlined' autoComplete='off' label='Phone Number' value={phone} onChange={(event) => setPhone(event.target.value)} />
+              <Button type='submit' variant='contained' sx={{ width: '240px', marginTop: '20px'}}>Send Code</Button>
+            </form>
+          </CardContent>
+        </Card>
+        <div id="recaptcha"></div>
+      </div>
+    ) 
+  } else {
+    return (
+      <div className='app__container'>
+        <Card sx={{ width: '300px'}}>
+          <CardContent sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <Typography sx={{ padding: '20px'}} variant='h5' component='div'>Enter the OTP</Typography>
+              <TextField sx={{ width: '240px'}} variant='outlined' label='OTP ' value={otp} onChange={verifyOtp} />
+          </CardContent>
+        </Card>
+        <div id="recaptcha"></div>
+      </div>
+    )
+  }
 }
 
-export default App
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,10 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuDyTmUEy4gY7Yeifs0r2a_yBFNgrf8vg",
-  authDomain: "react-firebase-376e4.firebaseapp.com",
-  projectId: "react-firebase-376e4",
-  storageBucket: "react-firebase-376e4.appspot.com",
-  messagingSenderId: "223030603375",
-  appId: "1:223030603375:web:795883137b3a587962e79a",
-  measurementId: "G-VHQNPWZL9P"
+  //config
 };
 
 const app = initializeApp(firebaseConfig);
 
-export default app;
+export const auth = getAuth(app);
